88e7d9461b171e0fee0731c017e98a99d87410ea
오예찬 <77969467+oycoyj2@users.noreply.github.com>
2023-08-08 01:00:50 +0900

    climb 추가

---
+++ climb/climb/src/test/java/com/climb/ClimbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.climb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
